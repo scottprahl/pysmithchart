4eeee474753d0322b3a62d6b10be3acdcabf1d75
Scott Prahl <scott@prahls.com>
2025-11-16 10:07:36 -0800

    update version

--- pysmithchart/__init__.py
+++ pysmithchart/__init__.py
@@ -42,9 +42,9 @@ register_projection(SmithAxes)
 # Public API for wildcard imports
 __all__ = ["SmithAxes", "S_PARAMETER", "Z_PARAMETER", "Y_PARAMETER"]
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __author__ = "Paul Staerke, Scott Prahl"
 __email__ = "scott.prahl@oit.edu"
 __copyright__ = "2025 Scott Prahl"
-__license__ = "BSD"
+__license__ = "BSD-3-Clause"
 __url__ = "https://github.com/scottprahl/pysmithchart.git"
