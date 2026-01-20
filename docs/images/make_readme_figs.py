cf2aaa3457e6246b16f791e4ab9001724b360566
Scott Prahl <scott@prahls.com>
2026-01-19 16:49:16 -0800

    use new API

--- docs/images/make_readme_figs.py
+++ docs/images/make_readme_figs.py
@@ -2,13 +2,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from pysmithchart import S_PARAMETER
+from pysmithchart import ABSOLUTE_DOMAIN
 
 S = [0.5 + 0.3j, -0.2 - 0.1j]
 
+sc = {"grid.major.color": "blue"}
 plt.figure(figsize=(6, 6))
-plt.subplot(1, 1, 1, projection="smith", grid_major_color_y="blue")
-plt.plot(S, ls="", markersize=10, datatype=S_PARAMETER)
+plt.subplot(1, 1, 1, projection="smith", **sc)
+plt.plot(S, ls="", markersize=10, domain=ABSOLUTE_DOMAIN)
 plt.title("Plotting Complex Reflection Coefficients")
 plt.savefig("readme_fig1.svg", format="svg")
 plt.show()
@@ -16,9 +17,10 @@ plt.show()
 
 ZL = [30 + 30j, 50 + 50j, 100 + 100j]
 
+sc = {"grid.minor.enable": True}
 plt.figure(figsize=(6, 6))
-plt.subplot(1, 1, 1, projection="smith", axes_impedance=200, grid_minor_enable=True)
-plt.plot(ZL, "b-o", markersize=10)  # default datatype is Z_PARAMETER
+plt.subplot(1, 1, 1, projection="smith", Z0=200, **sc)
+plt.plot(ZL, "b-o", markersize=10) 
 plt.title("Impedance parameters with Z₀=200Ω")
 plt.savefig("readme_fig2.svg", format="svg")
 plt.show()
@@ -28,7 +30,7 @@ ZL = [40 + 20j, 60 + 80j, 90 + 30j]
 
 plt.figure(figsize=(6, 6))
 plt.subplot(1, 1, 1, projection="smith")
-plt.plot(ZL, markersize=16, ls="--", markerhack=True, rotate_marker=True)
-plt.title("Custom markers")
+plt.plot(ZL, arrow=True)
+plt.title("Arrows")
 plt.savefig("readme_fig3.svg", format="svg")
 plt.show()
